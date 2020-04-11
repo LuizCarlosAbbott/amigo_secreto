a4962abc36865a2f90a609dc3397ce020e036330
LuizCarlosAbbott <luizcarlos_abbott@hotmail.com>
2020-04-11 08:49:22 -0300

    Updates

---
+++ precache-manifest.8015e2f50f48a12d691b03563d97f296.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4a8bb28b748e330ad74557fa1379f49",
+    "url": "/amigo_secreto/index.html"
+  },
+  {
+    "revision": "b9082309f5bf1fc4e7b7",
+    "url": "/amigo_secreto/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "e7da711b373d8c52e8aa",
+    "url": "/amigo_secreto/static/css/main.c0fe915c.chunk.css"
+  },
+  {
+    "revision": "b9082309f5bf1fc4e7b7",
+    "url": "/amigo_secreto/static/js/2.f67f9614.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/amigo_secreto/static/js/2.f67f9614.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7da711b373d8c52e8aa",
+    "url": "/amigo_secreto/static/js/main.54620187.chunk.js"
+  },
+  {
+    "revision": "4c0fa130c05a4b7bb26e",
+    "url": "/amigo_secreto/static/js/runtime-main.23bdfd5d.js"
+  }
+]);
